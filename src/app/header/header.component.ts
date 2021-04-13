b74a189b41d71b1225ec479c0ff730335658584c
ashwin <alexwin75@gmail.com>
2021-04-13 08:18:27 -0400

    adding in components, models

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+
+//decorator so angular knows this is a component
+@Component({
+selector:'app-header',
+templateUrl: './header.component.html'
+}
+)
+export class HeaderComponent {
+    collapsed = true;
+
+
+
+}
